9f3cf600d870009e016890e5a4b02c56d3bb477d
Cristian  CC <cristi.nord@gmail.com>
2018-10-20 22:11:26 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -125,5 +125,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-    programmer_hash[:alan_kay][:languages].push()
+    programmer_hash[:alan_kay][:languages].push("Assembly")
+    return programmer_hash
 end
